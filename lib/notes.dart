6e53d32ab8b3c32812b31f2dc9388bc737e84057
RakhaGalih <54633534+RakhaGalih@users.noreply.github.com>
2023-08-28 14:09:37 +0700

    adding styles

--- lib/notes.dart
+++ lib/notes.dart
@@ -28,6 +28,7 @@ class _NoteState extends State<Note> {
 
   @override
   Widget build(BuildContext context) {
+    Orientation orientation = MediaQuery.of(context).orientation;
     return Expanded(
       child: GestureDetector(
         onPanStart: (_) {
@@ -43,18 +44,65 @@ class _NoteState extends State<Note> {
           });
         },
         child: Padding(
-          padding: const EdgeInsets.all(8),
-          child: Container(
-              alignment: Alignment.center,
-              color: color,
-              child: Text(
-                widget.note,
-                style: const TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Color(0xFF151515)),
-              )),
-        ),
+            padding: (orientation == Orientation.portrait)
+                ? EdgeInsets.symmetric(vertical: 8, horizontal: widget.no * 5)
+                : EdgeInsets.fromLTRB(8, 8, 8, widget.no * 5),
+            child: Container(
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  color: color,
+                ),
+                child: (orientation == Orientation.portrait)
+                    ? Row(
+                        children: [
+                          const SizedBox(
+                            width: 30,
+                          ),
+                          const CircleAvatar(
+                            radius: 8,
+                            backgroundColor: Color(0xFF151515),
+                          ),
+                          const Spacer(),
+                          Text(
+                            widget.note,
+                            style: const TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Color(0xFF151515)),
+                          ),
+                          const Spacer(),
+                          const CircleAvatar(
+                            radius: 8,
+                            backgroundColor: Color(0xFF151515),
+                          ),
+                          const SizedBox(
+                            width: 30,
+                          ),
+                        ],
+                      )
+                    : Column(
+                        children: [
+                          const SizedBox(
+                            height: 30,
+                          ),
+                          Text(
+                            widget.note,
+                            style: const TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Color(0xFF151515)),
+                          ),
+                          const Spacer(),
+                          const CircleAvatar(
+                            radius: 8,
+                            backgroundColor: Color(0xFF151515),
+                          ),
+                          const SizedBox(
+                            height: 30,
+                          ),
+                        ],
+                      ))),
       ),
     );
   }
